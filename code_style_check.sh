99e27fa02c603e2d340da07514ddeb929a9beadc
sailor.zhou <sailor.zhou@cidana.com>
2019-03-18 15:38:17 +0800

    modify code_style_check.sh

--- code_style_check.sh
+++ code_style_check.sh
@@ -8,13 +8,13 @@
 ##git checkout -f ${sha1};
 commit_msg=$(git log --oneline -1);
 # commit_msg="5cd6d9f Merge c91688f9c04c2ada26f31eebf71fd812f2634beb into 43aedf77ce81a0121f767e02107f4b13779a9dfe";
-index_merge=$(expr index "$commit_msg" "Merge");
-index_into=$(expr index "$commit_msg" "into");
-commit_src=${commit_msg:$((index_merge+5)):$((index_into-index_merge-7))};
-commit_target=${commit_msg:$((index_into+4))};
+# commit_msg="ed6ae53 Merge 34f5e1a0f4cbd68a67633fa28d6e79790b14f617 into 43aedf77ce81a0121f767e02107f4b13779a9dfe";
+
+tmp=${commit_msg#*Merge};
+commit_src=${tmp%into*};
+commit_target=${commit_msg##*into};
 
 echo "commit_msg: $commit_msg";
-echo "index: $index";
 echo "commit_src: $commit_src";
 echo "commit_target: $commit_target";
 
